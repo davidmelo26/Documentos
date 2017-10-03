207fbf1502a4aa80ea37954cf7d08bfeb13796a7
David Melo <david.melo@utp.edu.co>
2017-10-02 22:46:45 -0500

    Subiendo actividad, investigacion JSON

---
+++ proyectos/BancoEjemplo1/src/bancoejemplo1/Banco.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancoejemplo1;
+
+/**
+ *
+ * @author ASUS
+ */
+//clase Banco
+public class Banco {
+ 
+    private String nombres;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+}
---
+++ proyectos/clases/src/QueuePriority/Menu.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package QueuePriority;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Menu {
+    
+    public static void main(String[] args){
+        
+        int op=0,a, b,r;
+        Scanner tecla = new Scanner(System.in);
+        
+        do{
+            System.out.println("1. opcion");
+            System.out.println("2 opcion");
+            System.out.println("3 opcion");
+            
+            op=tecla.nextInt();
+            
+            if(op==1){
+                System.out.println("Primer opcion");
+                
+            }else if(op==2){
+                System.out.println("segunda opcion");
+                
+            }else if(op==3){
+                System.out.println("tercer opcion");
+                
+            }
+        }while(op!=3);
+        }
+}
